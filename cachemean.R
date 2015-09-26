efc06f7ac72072b3ff27b9ac9d213c8f73ca4d88
prasun <prasunmax@gmail.com>
2015-09-27 00:11:37 +0530

    Commiting Assignment

---
+++ cachemean.R
@@ -0,0 +1,29 @@
+## Existing function given as sample
+##create Vector and the set of assignment
+# to use> a<-makeVector(1:20)
+makeVector <- function(x = numeric()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setmean <- function(mean) m <<- mean
+    getmean <- function() m
+    list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
+}
+#Caching functionality
+#to use> cachemean(a)
+cachemean <- function(x, ...) {
+    m <- x$getmean()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- mean(data, ...)
+    x$setmean(m)
+    m
+}
